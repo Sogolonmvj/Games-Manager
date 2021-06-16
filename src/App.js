f24a40ebb165e52ff3d36e0e90686c68237c1699
Sogolon <sogolonmvj@yahoo.com>
2021-06-15 23:42:39 -0300

    Initial Structure with Bootstrap, SASS and Reacr Router Dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import AdminLayout from './layouts/admin';
+
+import './style.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <Router>
+        <AdminLayout>
+          <Switch>
+            <Route path="/game/edit/:id">
+              <h1>Game Edit</h1>
+            </Route>
+            <Route path="/game/create">
+              <h1>Game Create</h1>
+            </Route>
+            <Route path="/game">
+              <h1>Game List</h1>
+            </Route>
+            <Route path="/">
+              <h1>Home</h1>
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/game">Game List</Link></li>
+                <li><Link to="/game/create">Game Create</Link></li>
+                <li><Link to="/game/edit/gfgffgdjgdhd">Game Edit</Link></li>
+              </ul>
+            </Route>
+          </Switch>
+        </AdminLayout>
+      </Router>
   );
 }
 
---
+++ src/layouts/admin.js
@@ -0,0 +1,12 @@
+const AdminLayout = ({children}) => {
+
+    return (
+        <div>
+            <h1>Admin Layout</h1>
+            {children}
+        </div>
+    )
+    
+};
+
+export default AdminLayout;
